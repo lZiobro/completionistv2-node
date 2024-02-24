e5a007a8344a921de3ef7e68e496b6f62b350271
lZiobro <303206@stud.umk.pl>
2024-02-24 18:40:40 +0100

    vercel 3.1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./api/server");
 
 const HOST = "https://completionistv2-node.vercel.app";
-const PORT = process.env.PORT || 21727;
+const PORT = process?.env?.PORT || 21727;
 
 server.listen(PORT, () => console.log(`Server running at ${HOST}:${PORT}`));

eeee00014acbfc18f3500ade97797c12e95bec4d
lZiobro <303206@stud.umk.pl>
2024-02-24 20:10:31 +0100

    another vercel fix try

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./api/server.js");
 
 const HOST = process?.env?.VERCEL_URL || "localhost";
-const PORT = process?.env?.PORT || 21727;
+const PORT = process?.env?.PORT || 80;
 
 server.listen(PORT, () => console.log(`Server running at ${HOST}:${PORT}`));
